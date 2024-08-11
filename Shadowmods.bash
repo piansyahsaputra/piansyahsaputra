e2b2b216ace5a5b1f05db3537d87bdac4b549ea7
piansyahsaputra <piansyahsaputra269@gmail.com>
2024-08-12 00:27:59 +0800

    Update Shadowmods.bash

--- Shadowmods.bash
+++ Shadowmods.bash
@@ -224,7 +224,7 @@ uninstall_theme() {
   echo -e "${BLUE}[+]                    DELETE THEME                 [+]${NC}"
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
   echo -e "                                                       "
-  bash <(curl https://raw.githubusercontent.com/VallzHost/installer-theme/main/repair.sh)
+  bash <(curl https://raw.githubusercontent.com/piansyahsaputra/piansyahsaputra/main/repair.sh)
   echo -e "                                                       "
   echo -e "${GREEN}[+] =============================================== [+]${NC}"
   echo -e "${GREEN}[+]                 DELETE THEME SUKSES             [+]${NC}"
